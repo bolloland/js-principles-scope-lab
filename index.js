5f493ece233aa86c0f724aab6babb64879df0790
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-05-20 20:24:00 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = "bob"
+customerName
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'scalise'
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'stubbykins'
+}
